fa6765fa07e805a1cebc31898d3263cf236d9a4d
Lucas Barbosa de Sousa <lucas.barbosa.sousa@ccc.ufcg.edu.br>
2021-09-10 10:11:35 -0300

    feat: include babel.config.js so preset is not need in cli

---
+++ babelTranspilation/babel.config.js
@@ -0,0 +1,8 @@
+// npx babel src --out-dir dist
+const presets = [
+    [
+        "@babel/preset-env"
+    ]
+];
+
+module.exports = { presets };
--- babelTranspilation/dist/testing-es8.js
+++ babelTranspilation/dist/testing-es8.js
@@ -1,6 +1,7 @@
 "use strict";
 
-// Preset-env converted it to ES5
+// Notice that in babel's NPX the comma after text2 is removed
+// in compiled
 function testingES8(text1, text2) {
   alert("".concat(text1, "\n").concat(text2));
 }
